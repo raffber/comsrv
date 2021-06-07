d59e03fe935be1c932fffd1dac6edc1312a82670
Raphael Bernhard <beraphae@gmail.com>
2021-06-07 22:42:38 +0200

    poke.comsrv.can: Documentation, allow skipping server reply

--- poke/comsrv/can.py
+++ poke/comsrv/can.py
@@ -37,6 +37,10 @@ class CanBus(object):
         self._device = device
 
     async def connect(self, url=None):
+        """
+        Connect to the server managing the CAN bus and start listening for RAW and GCT messages.
+        :param url: The URL to the communication server
+        """
         if self._client.connected:
             return self
         if url is None:
@@ -47,11 +51,23 @@ class CanBus(object):
         return self
 
     async def disconnect(self):
+        """ Disconnect from the server """
         await self.client.disconnect()
 
-    async def rpc(self, task):
+    async def rpc(self, task, rx_reply=True):
+        """
+        Perform an RPC to the CAN endpoint.
+
+        :param task: The message to be sent to the CAN endpoint.
+        :param rx_reply: True if a reply from the server should be awaited
+        :return: The return value of the server or None if `rx_reply=False`
+        """
         await self.connect()
-        resp = await self._client.request({'Can': {'addr': self._device, 'task': task}})
+        msg = {'Can': {'addr': self._device, 'task': task}}
+        if not rx_reply:
+            await self._client.send_request(msg)
+            return None
+        resp = await self._client.request(msg)
         if 'Error' in resp:
             if 'Can' in resp['Error']:
                 raise CanException(resp['Error']['Can'])
@@ -61,27 +77,41 @@ class CanBus(object):
             raise ComSrvException('Unexpected wire format')
         return resp['Can']
 
-    async def send(self, msg):
+    async def send(self, msg, rx_reply=True):
+        """
+        Send a CAN message. The message must be either of type `poke.can.CanMessage`
+        or `poke.can.GctMessage`.
+        Awaiting the reply from the server is not necessary and setting `rx_reply=False`
+        will disable this.
+
+        :param msg: The message to send to the bus
+        :param rx_reply: Wait for an acknowledgment of the server
+        """
         if isinstance(msg, CanMessage):
             task = {'TxRaw': msg.to_comsrv()}
         elif isinstance(msg, GctMessage):
             task = {'TxGct': msg.to_comsrv()}
         else:
             raise CanException('Invalid message type.')
-        await self.rpc(task)
+        await self.rpc(task, rx_reply=rx_reply)
 
     async def listen_raw(self):
+        """ Start listening to raw CAN messages on the bus """
         await self.rpc({'ListenRaw': True})
 
     async def listen_gct(self):
+        """ Start listening to GCT CAN messages on the bus """
         await self.rpc({'ListenGct': True})
 
     def raw(self):
+        """ Return a listener for raw CAN messages """
         return self._client.listen(raw_filter)
 
     def gct(self):
+        """ Return a listener for GCT CAN messages """
         return self._client.listen(gct_filter)
 
     @property
     def client(self):
+        """ Return the underlying RPC client """
         return self._client
