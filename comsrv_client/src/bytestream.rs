331331e82e075a794a1e2d242bb3d65ce26b731c
Raphael Bernhard <beraphae@gmail.com>
2021-09-19 23:21:22 +0200

    comsrv_client: More bytestream implementation

--- comsrv_client/src/bytestream.rs
+++ comsrv_client/src/bytestream.rs
@@ -1,7 +1,9 @@
-use crate::{lock, Lock, LockGuard, Locked, Rpc, DEFAULT_RPC_TIMEOUT};
+use std::time::Duration;
+
 use async_trait::async_trait;
 use comsrv_protocol::{ByteStreamRequest, ByteStreamResponse, Request, Response};
-use std::time::Duration;
+
+use crate::{lock, Lock, LockGuard, Locked, Rpc, DEFAULT_RPC_TIMEOUT};
 
 pub struct ByteStreamPipe<T: Rpc> {
     rpc: T,
@@ -38,21 +40,62 @@ impl<T: Rpc> ByteStreamPipe<T> {
         }
     }
 
-    pub async fn write(&mut self, data: &[u8]) -> crate::Result<()> {
+    pub async fn request(&mut self, task: ByteStreamRequest) -> crate::Result<ByteStreamResponse> {
         let ret = self
             .rpc
             .request(
                 Request::Bytes {
                     addr: self.addr.clone(),
-                    task: ByteStreamRequest::Write(data.to_vec()),
+                    task,
                     lock: self.lock.check_lock(),
                 },
                 self.timeout.clone(),
             )
             .await?;
         match ret {
-            Response::Bytes(ByteStreamResponse::Done) => Ok(()),
+            Response::Bytes(x) => Ok(x),
             Response::Error(x) => Err(crate::Error::from_rpc(x)),
+            _ => Err(crate::Error::UnexpectdResponse),
+        }
+    }
+
+    pub async fn write(&mut self, data: &[u8]) -> crate::Result<()> {
+        let req = ByteStreamRequest::Write(data.to_vec());
+        match self.request(req).await? {
+            ByteStreamResponse::Done => Ok(()),
+            _ => Err(crate::Error::UnexpectdResponse),
+        }
+    }
+
+    pub async fn read_all(&mut self) -> crate::Result<Vec<u8>> {
+        match self.request(ByteStreamRequest::ReadAll).await? {
+            ByteStreamResponse::Data(x) => Ok(x),
+            _ => Err(crate::Error::UnexpectdResponse),
+        }
+    }
+
+    pub async fn read_to_term(&mut self, term: u8, timeout: Duration) -> crate::Result<Vec<u8>> {
+        let timeout_ms = timeout.as_millis() as u32;
+        let req = ByteStreamRequest::ReadToTerm { term, timeout_ms };
+        match self.request(req).await? {
+            ByteStreamResponse::Data(x) => Ok(x),
+            _ => Err(crate::Error::UnexpectdResponse),
+        }
+    }
+
+    pub async fn read_exact(&mut self, count: u32, timeout: Duration) -> crate::Result<Vec<u8>> {
+        let timeout_ms = timeout.as_millis() as u32;
+        let req = ByteStreamRequest::ReadExact { count, timeout_ms };
+        match self.request(req).await? {
+            ByteStreamResponse::Data(x) => Ok(x),
+            _ => Err(crate::Error::UnexpectdResponse),
+        }
+    }
+
+    pub async fn read_upto(&mut self, count: u32) -> crate::Result<Vec<u8>> {
+        let req = ByteStreamRequest::ReadUpTo(count);
+        match self.request(req).await? {
+            ByteStreamResponse::Data(x) => Ok(x),
             _ => Err(crate::Error::UnexpectdResponse),
         }
     }
