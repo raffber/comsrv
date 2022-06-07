fea301de51d770d7b12e1f7e46c737cb292f9ac7
Raphael Bernhard <beraphae@gmail.com>
2022-06-08 00:40:07 +0200

    finally...

--- jenkins/comsrv-build.sh
+++ jenkins/comsrv-build.sh
@@ -16,22 +16,5 @@ useradd -u $user_id -g $group_id docker
 chown -R $user_id:$group_id /cargo /home /rust
 usermod -d /home docker
 
-(
-cat <<'EOF'
-#!/bin/bash
-
-set -euxfo pipefail
-
-export PATH="/cargo/bin:/rust/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-cd /data/comsrv
-
-cargo build --release
-cargo xwin build --target x86_64-pc-windows-msvc --release
-EOF
-) > /home/build.sh
-
-chmod +x /home/build.sh
-
-sudo -E -u docker /home/build.sh
-
+# sudo -E -u docker env PATH="$PATH" cargo build --release
+sudo -E -u docker env PATH="$PATH" cargo xwin build --target x86_64-pc-windows-msvc --release
