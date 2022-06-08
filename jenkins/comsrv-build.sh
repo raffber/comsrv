888d6799e3c26c8e29cfc35f2ac688c0be19b3a6
Raphael Bernhard <beraphae@gmail.com>
2022-06-08 11:50:59 +0200

    Improve CI setup

--- jenkins/comsrv-build.sh
+++ jenkins/comsrv-build.sh
@@ -2,6 +2,11 @@
 
 set -euxfo pipefail
 
+if ! [ -f /.dockerenv ]; then
+    echo "This script must run in a docker container. Otherwise it screws up your environment.";
+    exit 1;
+fi
+
 curdir=$(dirname "$0")
 rootdir=$(realpath "$curdir"/..)
 
@@ -16,5 +21,5 @@ useradd -u $user_id -g $group_id docker
 chown -R $user_id:$group_id /cargo /home /rust
 usermod -d /home docker
 
-# sudo -E -u docker env PATH="$PATH" cargo build --release
+sudo -E -u docker env PATH="$PATH" cargo build --release
 sudo -E -u docker env PATH="$PATH" cargo xwin build --target x86_64-pc-windows-msvc --release
