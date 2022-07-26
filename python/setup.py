c371972e5b4a45ef04254ce5c958d32c26b053c7
Raphael Bernhard <beraphae@gmail.com>
2022-07-26 12:15:29 +0200

    python: bump version

--- python/setup.py
+++ python/setup.py
@@ -3,30 +3,27 @@ import setuptools
 with open("../README.md") as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    data = f.read()
-
 requirements = [
     "aiohttp~=3.7",
     "numpy~=1.22",
-    "wsrpc @ git+https://github.com/raffber/wsrpc.git@release/v1.0.0#egg=wsrpc"
+    "wsrpc @ git+https://github.com/raffber/wsrpc.git@release/v1.0.0#egg=wsrpc",
 ]
 
 setuptools.setup(
     name="comsrv",
-    version="0.1.0",
+    version="0.2.0",
     author="Raphael Bernhard",
     author_email="beraphae@gmail.com",
     description="A communication relay for interfacing with instruments",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/raffber/comsrv",
-    packages=['comsrv'],
+    packages=["comsrv"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=requirements,
 )
