2aacf3f0ad8f483e281b7fa3277c09bd7eaacecf
Raphael Bernhard <beraphae@gmail.com>
2022-08-17 10:52:19 +0200

    ci: Update ci script

--- jenkins/build-ci.sh
+++ jenkins/build-ci.sh
@@ -3,6 +3,15 @@
 cd $(dirname "$0")
 cd ..
 
+rm -rf out
+mkdir out
+
 rm -rf target
 mv /cache/windows-release-cache target
 cargo build --release --target  x86_64-pc-windows-msvc
+cp target/x86_64-pc-windows-msvc/release/comsrv.exe out
+
+rm -rf target
+mv /cache/linux-release-cache target
+cargo build --release
+cp target/release/comsrv out
