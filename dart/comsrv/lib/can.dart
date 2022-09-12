47b0ca2da8adbc85c5bccb1f5c70c90a539c4bc1
Raphael Bernhard <beraphae@gmail.com>
2022-09-12 11:22:31 +0200

    dart: More work CAN client

--- dart/comsrv/lib/can.dart
+++ dart/comsrv/lib/can.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+import 'package:quiver/core.dart';
 
 import 'package:broadcast_wsrpc/lib.dart';
 import 'package:comsrv/comsrv.dart';
@@ -7,6 +8,24 @@ import 'package:comsrv/gctcan.dart';
 abstract class CanAddress extends Address {
   @override
   String get enumName => "Can";
+
+  CanAddress();
+
+  factory CanAddress.fromJson(JsonObject obj) {
+    if (obj.containsKey("PCan")) {
+      final addr = obj["PCan"];
+      return PCanAddress(addr["address"]);
+    } else if (obj.containsKey("SocketCan")) {
+      final addr = obj["SocketCan"];
+      return SocketCanAddress(addr["interface"]);
+    } else if (obj.containsKey("UsrCanet")) {
+      final addr = obj["UsrCanet"];
+      return UsrCanetAddress(addr["host"], addr["port"]);
+    } else if (obj.containsKey("Loopback")) {
+      return LoopBackAddress();
+    }
+    throw ArgumentError("Invalid JSON object.");
+  }
 }
 
 class PCanAddress extends CanAddress {
@@ -20,6 +39,17 @@ class PCanAddress extends CanAddress {
       "PCan": {"address": address}
     };
   }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! PCanAddress) {
+      return false;
+    }
+    return other.address == address;
+  }
+
+  @override
+  int get hashCode => address.hashCode;
 }
 
 class SocketCanAddress extends CanAddress {
@@ -33,6 +63,17 @@ class SocketCanAddress extends CanAddress {
       "SocketCan": {"interface": interface}
     };
   }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! SocketCanAddress) {
+      return false;
+    }
+    return other.interface == interface;
+  }
+
+  @override
+  int get hashCode => interface.hashCode;
 }
 
 class UsrCanetAddress extends CanAddress {
@@ -49,6 +90,17 @@ class UsrCanetAddress extends CanAddress {
       "UsrCanet": {"host": host, "port": port}
     };
   }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! UsrCanetAddress) {
+      return false;
+    }
+    return (other.host == host) && (other.port == port);
+  }
+
+  @override
+  int get hashCode => hash2(host, port);
 }
 
 class LoopBackAddress extends CanAddress {
@@ -56,6 +108,14 @@ class LoopBackAddress extends CanAddress {
   JsonObject toJson() {
     return {"Loopback": null};
   }
+
+  static final LoopBackAddress _singleton = LoopBackAddress._internal();
+
+  factory LoopBackAddress() {
+    return _singleton;
+  }
+
+  LoopBackAddress._internal();
 }
 
 class CanInstrument extends Instrument {
@@ -81,7 +141,18 @@ class CanInstrument extends Instrument {
   }
 }
 
-class CanMessage {}
+abstract class CanMessage {
+  CanMessage();
+
+  factory CanMessage.fromJson(JsonObject object) {
+    if (object.containsKey("Data")) {
+      return DataMessage.fromJson(object["Data"]);
+    } else if (object.containsKey("Remote")) {
+      return RemoteMessage.fromJson(object["Remote"]);
+    }
+    throw ArgumentError("Cannot deserialize CanMessage: Invalid format.");
+  }
+}
 
 class DataMessage extends CanMessage {
   final int id;
@@ -89,6 +160,13 @@ class DataMessage extends CanMessage {
   final Uint8List data;
 
   DataMessage(this.id, this.extendedId, this.data);
+
+  factory DataMessage.fromJson(JsonObject object) {
+    final id = object["id"];
+    final extendedId = object["ext_id"] as bool;
+    final data = Uint8List.fromList(object["data"] as List<int>);
+    return DataMessage(id, extendedId, data);
+  }
 }
 
 class RemoteMessage extends CanMessage {
@@ -97,12 +175,22 @@ class RemoteMessage extends CanMessage {
   final int dlc;
 
   RemoteMessage(this.id, this.extendedId, this.dlc);
+
+  factory RemoteMessage.fromJson(JsonObject object) {
+    final id = object["id"];
+    final extendedId = object["ext_id"] as bool;
+    final dlc = object["dlc"] as int;
+    return RemoteMessage(id, extendedId, dlc);
+  }
 }
 
 class CanBus extends BasePipe {
   CanInstrument instrument;
+  final WsRpc wsRpc;
 
-  CanBus(WsRpc rpc, this.instrument) : super(rpc);
+  CanBus(WsRpc rpc, this.instrument)
+      : wsRpc = rpc,
+        super(rpc);
 
   @override
   Future<JsonObject> request(JsonObject request) async {
@@ -118,19 +206,55 @@ class CanBus extends BasePipe {
     await request({"ListenRaw": true});
   }
 
+  Stream<JsonObject> _filterMessages() async* {
+    final client = await wsRpc.connect();
+    await for (final msg in client.messages()) {
+      if (msg.containsKey("Notify")) {
+        final notification = msg["Notify"] as JsonObject;
+        if (notification.containsKey("Can")) {
+          final canMsg = notification["Can"] as JsonObject;
+          final source = canMsg["source"] as JsonObject;
+          final sourceAddress = CanAddress.fromJson(source);
+          if (sourceAddress == instrument.address) {
+            yield canMsg["response"] as JsonObject;
+          }
+        }
+      }
+    }
+  }
+
   Stream<GctMessage> gctMessages() async* {
-    throw UnimplementedError();
+    await for (final msg in _filterMessages()) {
+      if (msg.containsKey("Gct")) {
+        final gctmsg = GctMessage.fromJson(msg["Gct"]);
+        if (gctmsg != null) {
+          yield gctmsg;
+        }
+      }
+    }
   }
 
   Stream<CanMessage> canMessages() async* {
-    throw UnimplementedError();
+    await for (final msg in _filterMessages()) {
+      if (msg.containsKey("Raw")) {
+        yield CanMessage.fromJson(msg["Raw"]);
+      }
+    }
   }
 
   Stream<RemoteMessage> remoteMessages() async* {
-    throw UnimplementedError();
+    await for (final msg in canMessages()) {
+      if (msg is RemoteMessage) {
+        yield msg;
+      }
+    }
   }
 
   Stream<DataMessage> dataMessages() async* {
-    throw UnimplementedError();
+    await for (final msg in canMessages()) {
+      if (msg is DataMessage) {
+        yield msg;
+      }
+    }
   }
 }
