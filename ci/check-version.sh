071a914f38e522a9603310c1217b272f52216c84
Raphael Bernhard <beraphae@gmail.com>
2024-04-24 11:43:55 +0200

    Fix version update script

--- ci/check-version.sh
+++ ci/check-version.sh
@@ -14,24 +14,36 @@ changelog_version=$(grep -m 1 -oE '## \[.*?\]' CHANGELOG.md | sed -e 's/[# \[]//
 client_version=$(grep -E -m 1 "^version" client/Cargo.toml | cut -d "=" -f 2 | sed 's/[" ]//g')
 protocol_version=$(grep -E -m 1 "^version" protocol/Cargo.toml | cut -d "=" -f 2 | sed 's/[" ]//g')
 py_version=$(grep -E -m 1 "^version" pyproject.toml | cut -d "=" -f 2 | sed 's/[" ]//g')
+dart_version=$(grep -E -m 1 "^version" dart/comsrv/pubspec.yaml | cut -d ":" -f 2 | sed 's/[" ]//g')
+
+fail=false
 
 if [[ $version != $changelog_version ]]; then
     echo "Invalid version in changelog. Found: ${changelog_version}"
-    exit 1
+    fail=true
 fi
 
 if [[ $version != $client_version ]]; then
     echo "Invalid version in client. Found: ${client_version}"
-    exit 1
+    fail=true
 fi
 
 if [[ $version != $protocol_version ]]; then
     echo "Invalid version in protocol. Found: ${protocol_version}"
-    exit 1
+    fail=true
 fi
 
 if [[ $version != $py_version ]]; then
     echo "Invalid version in python package. Found: ${py_version}"
+    fail=true
+fi
+
+if [[ $version != $dart_version ]]; then
+    echo "Invalid version in dart package. Found: ${dart_version}"
+    fail=true
+fi
+
+if [[ $fail == true ]]; then
     exit 1
 fi
 
--- ci/update-version.sh
+++ ci/update-version.sh
@@ -1,12 +1,54 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 cd $(dirname "$0")/..
 
 version=$(./ci/get-version.sh)
 
-rg '^version = \"[\d\.]+"' --iglob 'pyproject.lock' --iglob "Cargo.toml" -m 1 -r "version = \"${version}\"" -q
-rg '^version: [\d\.]+' --iglob 'pubspec.yaml' -m 1 -r "version: ${version}" -q
+replace_version_toml() {
+
+python_script=$(cat <<EOF
+import re
+
+with open("$1", "r") as f:
+    content = f.read()
+
+content = re.sub(r'version = "[\d\.]+"', 'version = "${version}"', content, count=1)
+
+with open("$1", "w") as f:
+    f.write(content)
+EOF
+)
+
+python -c "$python_script"
+
+}
+
+replace_version_yaml() {
+
+python_script=$(cat <<EOF
+import re
+
+with open("$1", "r") as f:
+    content = f.read()
+
+content = re.sub(r'version: [\d\.]+', 'version: ${version}', content, count=1)
+
+with open("$1", "w") as f:
+    f.write(content)
+EOF
+)
+
+python -c "$python_script"
+
+}
+
+
+replace_version_toml client/Cargo.toml
+replace_version_toml protocol/Cargo.toml
+replace_version_toml comsrv/Cargo.toml
+replace_version_toml pyproject.toml
+replace_version_yaml dart/comsrv/pubspec.yaml
 
 ./ci/check-version.sh
